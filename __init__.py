882f6844e05349fd62aae391d14dd993983a5253
SimonDaNinja <simon@simonssoffa.xyz>
2023-12-26 21:06:09 +0100

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,60 @@
+from .Adds import \
+        Direction, \
+        Adder, \
+        HeadElementAdder, \
+        BodyElementAdder, \
+        PageContentAdder, \
+        LinkMenuAdder, \
+        CategoryMenuAdder, \
+        CategoryFullMenuAdder, \
+        CategoryBriefMenuAdder, \
+        HtmlElementAdder, \
+        DoctypeElementAdder, \
+        PageFullTitleH1Adder, \
+        PageFullTitleH2Adder, \
+        PageFullTitleH3Adder, \
+        PageBriefTitleH1Adder, \
+        PageBriefTitleH2Adder, \
+        PageBriefTitleH3Adder, \
+        WebsiteFullTitleH1Adder, \
+        WebsiteFullTitleH2Adder, \
+        WebsiteFullTitleH3Adder, \
+        WebsiteBriefTitleH1Adder, \
+        WebsiteBriefTitleH2Adder, \
+        WebsiteBriefTitleH3Adder, \
+        NavigationHelperAdder, \
+        RssElementAdder, \
+        RssChannelElementAdder, \
+        RssChannelTitleElementAdder, \
+        RssChannelDescriptionElementAdder, \
+        RssChannelLinkElementAdder
+
+from .constants import \
+        WEBSITE_PATH \
+        INDEX_PATH \
+        APPLICATION_NAME
+
+from .ContentBuilder import \
+        ContentBuilder,
+        ContentElement,
+        StringElement,
+        NullElement,
+        HtmlElement
+
+from .LinkMenu import \
+        LinkMenuElement, \
+        BulletLinkMenuElement, \
+        LegacySoffanTopbarMenuElement, \
+        LinkMenuItem
+
+from .Page import
+        Page, \
+        CategoryPage, \
+        RssAblePage, \
+        BlogPage, \
+        RssPage, \
+        RssItem
+
+from .PageCategory import PageCategory
+
+from .WebsiteBuilder import WebsiteBuilder
